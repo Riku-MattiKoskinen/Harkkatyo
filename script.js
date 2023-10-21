6e61308b6b89a1ef2ab1cf13696b2c4b995814f4
Riku <72088238+Riku-MattiKoskinen@users.noreply.github.com>
2023-10-21 12:53:43 +0300

    Poistettu painike tauluk. paiv. ja lis. autom.paiv

--- script.js
+++ script.js
@@ -1,384 +1,75 @@
-/*const jsonQuery1 = {
-    "query": [
-      {
-        "code": "Vuosi",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "2023"
-          ]
-        }
-      },
-      {
-        "code": "Sukupuoli",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "SSS"
-          ]
-        }
-      },
-      {
-        "code": "Puolue",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "03",
-            "02",
-            "01",
-            "04",
-            "05",
-            "06",
-            "07",
-            "08",
-            "09"
-          ]
-        }
-      },
-      {
-        "code": "Vaalipiiri ja kunta vaalivuonna",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "010091",
-            "020018",
-            "020049",
-            "020078",
-            "020092",
-            "020106",
-            "020149",
-            "020186",
-            "020224",
-            "020235",
-            "020245",
-            "020257",
-            "020407",
-            "020434",
-            "020444",
-            "020504",
-            "020505",
-            "020543",
-            "020611",
-            "020616",
-            "020638",
-            "020710",
-            "020753",
-            "020755",
-            "020858",
-            "020927",
-            "030019",
-            "030202",
-            "030284",
-            "030304",
-            "030322",
-            "030923",
-            "030400",
-            "030423",
-            "030430",
-            "030445",
-            "030480",
-            "030481",
-            "030503",
-            "030529",
-            "030920",
-            "030538",
-            "030561",
-            "030577",
-            "030631",
-            "030636",
-            "030979",
-            "030680",
-            "030704",
-            "030906",
-            "030734",
-            "030738",
-            "030761",
-            "030833",
-            "030853",
-            "030895",
-            "030918",
-            "040050",
-            "040051",
-            "040079",
-            "040102",
-            "040913",
-            "040181",
-            "040214",
-            "040230",
-            "040271",
-            "040484",
-            "040531",
-            "040608",
-            "040609",
-            "040684",
-            "040747",
-            "040783",
-            "040886",
-            "060016",
-            "060061",
-            "060081",
-            "060082",
-            "060086",
-            "060098",
-            "060103",
-            "060109",
-            "060111",
-            "060142",
-            "060165",
-            "060169",
-            "060316",
-            "060398",
-            "060433",
-            "060560",
-            "060576",
-            "060694",
-            "060781",
-            "060834",
-            "060981",
-            "070020",
-            "070928",
-            "070108",
-            "070143",
-            "070177",
-            "070211",
-            "070250",
-            "070291",
-            "070418",
-            "070508",
-            "070933",
-            "070536",
-            "070562",
-            "070581",
-            "070604",
-            "070619",
-            "070635",
-            "070702",
-            "070790",
-            "070912",
-            "070988",
-            "070837",
-            "070887",
-            "070908",
-            "070922",
-            "070936",
-            "070980",
-            "070932",
-            "080046",
-            "080075",
-            "080917",
-            "080097",
-            "080153",
-            "080178",
-            "080213",
-            "080285",
-            "080286",
-            "080909",
-            "080405",
-            "080978",
-            "080416",
-            "080441",
-            "080489",
-            "080491",
-            "080507",
-            "080580",
-            "080588",
-            "080593",
-            "080937",
-            "080623",
-            "080624",
-            "080681",
-            "080689",
-            "080700",
-            "080739",
-            "080740",
-            "080768",
-            "080831",
-            "080935",
-            "090090",
-            "090140",
-            "090146",
-            "090167",
-            "090171",
-            "090176",
-            "090204",
-            "090239",
-            "090260",
-            "090263",
-            "090276",
-            "090297",
-            "090919",
-            "090309",
-            "090402",
-            "090916",
-            "090420",
-            "090422",
-            "090426",
-            "090541",
-            "090911",
-            "090595",
-            "090607",
-            "090686",
-            "090687",
-            "090707",
-            "090749",
-            "090762",
-            "090778",
-            "090844",
-            "090848",
-            "090943",
-            "090857",
-            "090915",
-            "090921",
-            "090925",
-            "100000",
-            "100005",
-            "100010",
-            "100052",
-            "100074",
-            "100145",
-            "100151",
-            "100152",
-            "100217",
-            "100218",
-            "100231",
-            "100232",
-            "100233",
-            "100971",
-            "100236",
-            "100272",
-            "100280",
-            "100287",
-            "100288",
-            "100300",
-            "100301",
-            "100399",
-            "100403",
-            "100408",
-            "100421",
-            "100440",
-            "100475",
-            "100499",
-            "100545",
-            "100584",
-            "100598",
-            "100599",
-            "100743",
-            "100975",
-            "100759",
-            "100846",
-            "100849",
-            "100893",
-            "100905",
-            "100942",
-            "100924",
-            "100934",
-            "100946",
-            "100945",
-            "100944",
-            "100989",
-            "110077",
-            "110172",
-            "110179",
-            "110182",
-            "110216",
-            "110226",
-            "110249",
-            "110256",
-            "110265",
-            "110275",
-            "110312",
-            "110410",
-            "110435",
-            "110495",
-            "110500",
-            "110592",
-            "110601",
-            "110729",
-            "110850",
-            "110892",
-            "110931",
-            "110992",
-            "120000",
-            "120009",
-            "120069",
-            "120071",
-            "120072",
-            "120105",
-            "120139",
-            "120205",
-            "120940",
-            "120208",
-            "120244",
-            "120290",
-            "120305",
-            "120317",
-            "120425",
-            "120436",
-            "120483",
-            "120494",
-            "120535",
-            "120563",
-            "120564",
-            "120973",
-            "120972",
-            "120578",
-            "120615",
-            "120620",
-            "120625",
-            "120626",
-            "120630",
-            "120678",
-            "120926",
-            "120691",
-            "120697",
-            "120746",
-            "120748",
-            "120765",
-            "120777",
-            "120785",
-            "120791",
-            "120832",
-            "120859",
-            "120889",
-            "120977",
-            "130047",
-            "130148",
-            "130240",
-            "130241",
-            "130261",
-            "130273",
-            "130320",
-            "130498",
-            "130583",
-            "130614",
-            "130683",
-            "130698",
-            "130732",
-            "130742",
-            "130751",
-            "130758",
-            "130845",
-            "130851",
-            "130854",
-            "130890",
-            "130976"
-          ]
-        }
-      },
-      {
-        "code": "Tiedot",
-        "selection": {
-          "filter": "item",
-          "values": [
-            "evaa_osuus_aanista"
-          ]
-        }
+const jsonQuery1 = {
+  "query": [
+    {
+      "code": "Vuosi",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "2023"
+        ]
       }
-    ],
-    "response": {
-      "format": "json-stat2"
+    },
+    {
+      "code": "Sukupuoli",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "SSS"
+        ]
+      }
+    },
+    {
+      "code": "Puolue",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "03",
+          "02",
+          "01",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09"
+        ]
+      }
+    },
+    {
+      "code": "Vaalipiiri ja kunta vaalivuonna",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "010000",
+          "020000",
+          "030000",
+          "040000",
+          "060000",
+          "070000",
+          "080000",
+          "090000",
+          "100000",
+          "110000",
+          "120000",
+          "130000",
+          "050000"
+        ]
+      }
+    },
+    {
+      "code": "Tiedot",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "evaa_osuus_aanista"
+        ]
+      }
     }
-}*/
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+}
 
 const jsonQuery2 = {
     "query": [
@@ -453,7 +144,7 @@ const jsonQuery2 = {
     }
   }
 
-/*const getData  = async () => {
+const getData  = async () => {
     const url = "https://statfin.stat.fi:443/PxWeb/api/v1/fi/StatFin/evaa/statfin_evaa_pxt_13sw.px"
     const res = await fetch(url, {
         method: "POST",
@@ -480,11 +171,11 @@ const populateDropdown = (municipalities) => {
 };
 
 
-const buildChart = async () => {
+const buildChart = async (chosenMunicipality) => {
     const data = await getData()
     const parties = Object.values(data.dimension.Puolue.category.label);
     const labelName = "Vaalipiiri ja kunta vaalivuonna";
-    const chosenMunicipality = "KU071 Haapavesi";
+    //const chosenMunicipality = "VP01 Helsingin vaalipiiri";
 
     const allMunicipalities = Object.values(data.dimension[labelName].category.label);
     const chosenIndex = allMunicipalities.indexOf(chosenMunicipality);
@@ -533,12 +224,17 @@ getData().then(data => {
     buildChart("KU071 Haapavesi"); // Default municipality
 });
 
-document.getElementById("update-chart").addEventListener("click", () => {
+/*document.getElementById("update-chart").addEventListener("click", () => {
     const selectedMunicipality = document.getElementById("municipalities").value;
     buildChart(selectedMunicipality);
 });*/
 
-// Second chart starts here
+document.getElementById("municipalities").addEventListener("change", () => {
+    const selectedMunicipality = document.getElementById("municipalities").value;
+    buildChart(selectedMunicipality);
+});
+
+/* Second chart starts here
 const getCandidateData = async () => {
     const url = "https://statfin.stat.fi:443/PxWeb/api/v1/fi/StatFin/evaa/statfin_evaa_pxt_13sm.px"
 
@@ -625,10 +321,14 @@ getCandidateData().then(data => {
     buildCandidateChart("KU071 Haapavesi"); // Default municipality
 });
 
-document.getElementById("update-chart").addEventListener("click", () => {
-    const chosenRegion = document.getElementById("municipalities").value;
-    buildCandidateChart(chosenRegion);
+//document.getElementById("update-chart").addEventListener("click", () => {
+//    const chosenRegion = document.getElementById("municipalities").value;
+//    buildCandidateChart(chosenRegion);
+//});
+
+document.getElementById("municipalities").addEventListener("change", () => {
+    const selectedMunicipality = document.getElementById("municipalities").value;
+    buildChart(selectedMunicipality);
 });
 
-
-buildCandidateChart()
+*/
